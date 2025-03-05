76ea10edff69dd85c052a506c9e4cc8ffd343343
Ali Reza Eshaqi <eshaqi.alireza@gmail.com>
2025-03-05 14:34:49 +1100

    update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = require('./app');
 
 connectToDatabse();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
 });
