43c59575a218e20cb0ef749c5b5c51ee85059ed7
Ali Reza Eshaqi <eshaqi.alireza@gmail.com>
2025-03-06 11:24:10 +1100

    Implemented CORS

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -24,6 +25,20 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+// Implement CORS
+app.use(cors());
+
+// Access-Control-Allow-Origin *
+// if api and front end are separte
+// api: api.natours.com
+// front-end: natours.com
+/* app.use(cors({
+  origin: 'https://www.natours.com'
+})) */
+
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors())
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
